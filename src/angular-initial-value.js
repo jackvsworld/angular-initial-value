23d655bd06a4583f74fbf975baf068c57a6438c6
jackvsworld <ajbarton@users.sf.net>
2015-01-20 22:48:36 -0800

    jsodc
    attribution

--- src/angular-initial-value.js
+++ src/angular-initial-value.js
@@ -1,3 +1,9 @@
+/*!*
+ * @file angular-initial-value.js
+ * @version 1.0.0
+ * @author Andrew Barton <andrew@smithcart.com>
+ * @copyright © 2015 Andrew Barton
+ */
 angular.module("initialValue", [])
   .directive("initialValue", [function () {
     return {
